bd2fb383afc6a2ef4bea8145e6f4c538de21f50d
Tawicha Sirisuwan <tawicha9643@outlook.com>
2025-04-23 09:31:11 +0700

    Navbar change

--- client/src/components/Navbar.js
+++ client/src/components/Navbar.js
@@ -2,6 +2,8 @@ import React from "react";
 import'./Navbar.css'
 
 function Navbar() {
+
+
   return (
   <body>
     <nav>
@@ -10,18 +12,12 @@ function Navbar() {
                 <li><a href=""><img src="/image/Logo.png" alt="Logo"></img></a></li>
             </ul>    
         </section>
-        <section id="links">
-             
-        </section>
-        {/* <section id="search">
-           <input type="search" placeholder=" Search for Product... "></input>
-        </section> */}
         <section  id="account">
           <ul>
             <li><a href=""><img src="/image/bell.png" alt="Shopping-Cart"></img></a></li>
-            <li><a href=""><img src="/image/user_profile.png" alt="profile-user"></img></a></li>
+            <li><a href=""><img id="" src="/image/user_profile.png" alt="profile-user"></img></a></li>
             <li><a href=""><strong>Admin STI</strong></a></li>
-            <li><a href=""><img src="/image/exit.png" alt="profile-user"></img></a></li>
+            <li><a href=""><img src="/image/exit.png" alt="exit-logout"></img></a></li>
             <li><a href="">Logout</a></li>
           </ul>    
         </section>
